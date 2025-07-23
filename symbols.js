852667409ff59f095ec78d0c3e7754e7b825d235
JUPITERBYRD <thisisjupiterbyrd@gmail.com>
2025-07-23 07:19:46 +0200

    Create symbols.js

---
+++ symbols.js
@@ -0,0 +1,131 @@
+const TWO_SQUARE = [
+  { x: -150, y: -150 },
+  { x: 0, y: -150 },
+  { x: 0, y: 0 },
+  { x: 0, y: 150 },
+  { x: 150, y: 150 },
+
+  { x: 150, y: 0 },
+  { x: -150, y: 0 },
+];
+
+const FOUR_SQUARE = [
+  { x: -150, y: -150 },
+  { x: -75, y: -150 },
+  { x: -75, y: -75 },
+  { x: -75, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 75 },
+  { x: 75, y: 75 },
+  { x: 75, y: 150 },
+  { x: 150, y: 150 },
+  { x: 150, y: 75 },
+  { x: 75, y: 75 },
+  { x: 75, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: -75 },
+  { x: -75, y: -75 },
+  { x: -150, y: -75 },
+];
+
+const THREE_SQUARE = [
+  { x: -75, y: -75 },
+  { x: -75, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: 75 },
+  { x: 75, y: 75 },
+  { x: 75, y: 150 },
+  { x: 150, y: 150 },
+  { x: 150, y: 75 },
+  { x: 75, y: 75 },
+  { x: 75, y: 0 },
+  { x: 0, y: 0 },
+  { x: 0, y: -75 },
+];
+
+const CLASSIC_MAGNET = [
+  { x: -150, y: -75 },
+  { x: 100, y: -75 },
+  { x: 100, y: 0 },
+  { x: -75, y: 0 },
+  { x: -75, y: 75 },
+  { x: 100, y: 75 },
+  { x: 100, y: 150 },
+  { x: -150, y: 150 },
+  { x: -150, y: 75 },
+];
+
+const TILTED_SQUARE_rollingGearRadius = 67;
+const TILTED_SQUARE_penOffset = 70;
+
+//context.clearRect(0, 0, canvas.width, canvas.height);
+// Square stationary gear
+
+const TILTED_SQUARE = [
+  { x: 0, y: 150 },
+  { x: 150, y: 0 },
+  { x: 0, y: -150 },
+
+  { x: -150, y: 0 },
+];
+
+const ICECREAM_STICK_rollingGearRadius = 86;
+const ICECREAM_STICK_penOffset = 90;
+
+//context.clearRect(0, 0, canvas.width, canvas.height);
+// Square stationary gear
+
+const ICECREAM_STICK = [
+  { x: 0, y: 150 },
+
+  { x: 0, y: -150 },
+];
+
+const THREE_STAR_rollingGearRadius = 66;
+const THREE_STAR_penOffset = 70;
+
+//context.clearRect(0, 0, canvas.width, canvas.height);
+// Square stationary gear
+
+const THREE_STAR = [
+  { x: 150, y: 150 },
+  { x: 75, y: -150 },
+  { x: 0, y: 150 },
+  { x: -75, y: -150 },
+  { x: -150, y: 150 },
+];
+
+const UPSIDE_DOWN_HEART_rollingGearRadius = 55;
+const UPSIDE_DOWN_HEART_penOffset = 60;
+
+//context.clearRect(0, 0, canvas.width, canvas.height);
+// Square stationary gear
+
+const UPSIDE_DOWN_HEART = [
+  { x: 150, y: 150 },
+  { x: 75, y: -150 },
+  { x: 0, y: 150 },
+  { x: -75, y: -150 },
+  { x: -150, y: 150 },
+];
+
+const ISOSCELES_rollingGearRadius = 49;
+const ISOSCELES_penOffset = 50;
+
+const ISOSCELES = [
+  { x: 150, y: 150 },
+  { x: 0, y: -150 },
+  { x: -150, y: 150 },
+];
+
+const rollingGearRadius = 44;
+const penOffset = 50;
+
+const square = [
+  { x: 70, y: 70 },
+  { x: 40, y: 0 },
+  { x: 0, y: -150 },
+  { x: -40, y: 0 },
+  { x: -70, y: 70 },
+  { x: 0, y: 40 },
+];
